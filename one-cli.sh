8f53654bda2c34a448b52256f84ef3c5268a3177
Anton Samarin <Anton.Samarin@jetbrains.com>
2024-05-16 17:41:00 +0200

    Fix

--- one-cli.sh
+++
@@ -1,3 +0,0 @@
-qodana scan \
--e QODANA_TOKEN="eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJvcmdhbml6YXRpb24iOiJwOTE2aiIsInByb2plY3QiOiJBZ3ltayIsInRva2VuIjoiQUduRHgifQ.1QcC-7S7M9y8eFm2hE6q7O3nBbsc_OcF3t-L7-LL5So" \
---profile-path ./mp.yaml
